ce9704eac818ab1607ffe6501130884f674e5c7b
Daniel Tello <daniel.tello@usil.pe>
2021-11-23 00:34:36 -0500

    agregamos css y java

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('hola me falto este mensaje')
+document.querySelector('.menu-btn').addEventListener('click', ()=> {
+    document.querySelector('.nav-menu').classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {  delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', {  delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', {  delay: 500 });
