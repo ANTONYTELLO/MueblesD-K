a1538983c07ccd347b6ace0822293dee911f8503
Daniel <danieltelloa@hotmail.com>
2025-09-28 02:14:04 -0500

    segundo

---
+++ scrollreveal/scrollconfi.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal();
+
+     sr.reveal('.section-p11', {
+        duration: 2000,
+     });
+     sr.reveal('.section-p1', {
+        duration: 2000,
+     });
+     sr.reveal('.section-p111', {
+        duration: 2000,
+     });
+     sr.reveal('.banner-box', {
+      duration: 2000,
+     });
+     sr.reveal('.footer-mapa', {
+      duration: 2000,
+     });
