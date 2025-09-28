76c338d26105ea4f71f897ab080e771bc40f0af1
Daniel <danieltelloa@hotmail.com>
2025-09-28 02:16:26 -0500

    cuarto

---
+++ js/filtro.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('.filter').click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        let valor = $(this).attr('data-nombre');
+        if (valor == 'todos') {
+            $('.cont-work').show('1000');
+        } else {
+            $('.cont-work').not('.' + valor).hide('1000');
+            $('.cont-work').filter('.' + valor).show('1000');
+        }
+    });
+
+});
